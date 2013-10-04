f4e41946bc674d4e9027c947ac577eaf4fc4447c
Mabille <gabi.mabs@gmail.com>
2013-10-04 13:21:27 -0300

    Changed a bit code of DatabaseProyectoModel to make it more readable

--- DatabaseProyecto/src/DatabaseProyectoModel.java
+++ DatabaseProyecto/src/DatabaseProyectoModel.java
@@ -5,11 +5,11 @@ public class DatabaseProyectoModel {
 	public static void main(String[] args) {
 		System.out.println("Proyecto de Base De Datos I y medio");
 		System.out.println("Justine Compagnon | Gabriel Mabille");
-		
+
 		try {
 			Class.forName("org.postgresql.Driver");
 			System.out.println("Driver O.K.");
-			
+
 		} catch ( ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -17,13 +17,16 @@ public class DatabaseProyectoModel {
 
 		Connection connect;
 		try {
-			connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/proyecto","postgres", "root");
+			String url = "jdbc:postgresql://localhost:5432/proyecto";
+			String user = "postgres";
+			String passwd = "root";
+			connect = DriverManager.getConnection(url,user,passwd);
 			System.out.println("Connection Succeeded.");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
