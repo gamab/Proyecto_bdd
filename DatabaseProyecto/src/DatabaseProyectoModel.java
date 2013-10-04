b62b9b906be99343c8e261c9d96554de037fccf9
Mabille <gabi.mabs@gmail.com>
2013-10-03 23:45:10 -0300

    Added Java Project Files

---
+++ DatabaseProyecto/src/DatabaseProyectoModel.java
@@ -0,0 +1,9 @@
+
+public class DatabaseProyectoModel {
+
+	public static void main(String[] args) {
+		System.out.println("Proyecto de Base De Datos I y medio");
+		System.out.println("Justine Compagnon | Gabriel Mabille");
+	}
+	
+}
