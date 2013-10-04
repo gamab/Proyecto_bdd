68ae29c97403d75745521f697727b0a661c425b9
Mabille <gabi.mabs@gmail.com>
2013-10-04 12:29:07 -0300

    Modified a bi the DatabaseProyectoModel so that it runs with the jdbc driver

--- DatabaseProyecto/src/DatabaseProyectoModel.java
+++ DatabaseProyecto/src/DatabaseProyectoModel.java
@@ -1,9 +1,22 @@
+import java.sql.*;
 
 public class DatabaseProyectoModel {
 
 	public static void main(String[] args) {
 		System.out.println("Proyecto de Base De Datos I y medio");
 		System.out.println("Justine Compagnon | Gabriel Mabille");
+		
+		Connection connection;
+		try {
+			Class.forName("org.postgresql.Driver");
+			
+			System.out.print("Driver O.K.");
+			
+		} catch ( ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
