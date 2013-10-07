6b718a20d347ff2da2a9b5b4940d5cc7c9656ce1
Mabille <gabi.mabs@gmail.com>
2013-10-07 15:06:14 -0300

    Create API for MiddlePanel

--- DatabaseProyecto/src/ar/proyecto/gui/MiddlePanel.java
+++ DatabaseProyecto/src/ar/proyecto/gui/MiddlePanel.java
@@ -1,7 +1,48 @@
 package ar.proyecto.gui;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
-public class MiddlePanel extends JPanel {
+public abstract class MiddlePanel extends JPanel {
+	//Para crear el panel
+	protected Color backgroundColor;
+	protected int height;
+	protected int width;
+	
+	protected MiddlePanel() {
+		
+	}
 
+	protected abstract void paintComponent(Graphics g);
+	
+	public Color getBackgroundColor() {
+		return backgroundColor;
+	}
+
+	public void setBackgroundColor(Color backgroundColor) {
+		this.backgroundColor = backgroundColor;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public void setSize(int height, int width) {
+		this.height = height;
+		this.width = width;
+	}
 }
