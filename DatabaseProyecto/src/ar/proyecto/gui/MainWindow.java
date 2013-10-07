55c5647964b696ac027db5fc28325c11eada504c
Mabille <gabi.mabs@gmail.com>
2013-10-07 14:49:26 -0300

    Create API for MainWindow

--- DatabaseProyecto/src/ar/proyecto/gui/MainWindow.java
+++ DatabaseProyecto/src/ar/proyecto/gui/MainWindow.java
@@ -1,7 +1,57 @@
 package ar.proyecto.gui;
 
+import java.sql.ResultSet;
+
 import javax.swing.JFrame;
 
-public class MainWindow extends JFrame {
+import ar.proyecto.controller.Controller;
+import ar.proyecto.controller.RequestType;
 
+public class MainWindow extends JFrame {
+	
+	//Para construir la ventana
+	TopPanel panelTop;
+	MiddlePanel panelMiddle;
+	BottomPanel panelBottom;
+	String title;
+	int width;
+	int height;
+	
+	//Para comunicar con el modelo
+	Controller controller;
+	
+	
+	//-------------------------------------
+	//Para construir y modificar la ventana
+	//-------------------------------------
+	//Constructor
+	public MainWindow() {
+		
+	}
+	
+	//Para cambiar el panel del medio
+	public void setMiddlePanel(MiddlePanel panel) {
+		
+	}
+	
+	//Para mostrar un resultado en el panel de abajo
+	public void setResult(String result) {
+		
+	}
+	//Para mostrar un resultado en forma de una tablita
+	public void setResult(ResultSet result) {
+		
+	}
+	
+	//----------------------------
+	//Para comunicar con el modelo
+	//----------------------------
+	public void setController(Controller ctrllr) {
+		this.controller = ctrllr;
+	}
+	
+	public void sendRequestToController(String request, RequestType type) {
+		
+	}
+	
 }
