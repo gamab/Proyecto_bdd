a0ada218f0d18249ee0fba152b8edd255b8e9fbe
Mabille <gabi.mabs@gmail.com>
2013-10-07 14:49:58 -0300

    Create API for Controller

--- DatabaseProyecto/src/ar/proyecto/controller/Controller.java
+++ DatabaseProyecto/src/ar/proyecto/controller/Controller.java
@@ -1,5 +1,22 @@
 package ar.proyecto.controller;
 
-public class Controller {
+import ar.proyecto.gui.MainWindow;
+import ar.proyecto.model.DatabaseConnection;
 
+public class Controller {
+	//Para utilizar el modelo
+	DatabaseConnection model;
+	//Para modificar la GUI
+	MainWindow gui;
+	
+	
+	//Creador
+	public Controller() {
+		
+	}
+	
+	//Executar una consulta y modificar la gui
+	public void sendRequestToModelAndUpdateGui(String request, RequestType type) {
+		
+	}
 }
