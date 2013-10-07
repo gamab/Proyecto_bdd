95c8c33574309517c3028f8e72451a42070110d0
Mabille <gabi.mabs@gmail.com>
2013-10-07 15:45:07 -0300

    Created API for MiddlePanelSelect

--- DatabaseProyecto/src/ar/proyecto/gui/MiddlePanelSelect.java
+++ DatabaseProyecto/src/ar/proyecto/gui/MiddlePanelSelect.java
@@ -1,5 +1,41 @@
 package ar.proyecto.gui;
 
+import java.awt.Graphics;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class MiddlePanelSelect extends MiddlePanel {
+	private JLabel from;
+	private JComboBox cbTable;
+	private JLabel whereNum;
+	private JTextField nroMulta;
+		
+	private JButton ok;
+
+	//Para communicar con la gui
+	private MainWindow gui;
+
+
+	//constructor
+	public MiddlePanelSelect() {
+		super();		
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public MainWindow getGui() {
+		return gui;
+	}
+
+	public void setGui(MainWindow gui) {
+		this.gui = gui;
+	}
 
 }
