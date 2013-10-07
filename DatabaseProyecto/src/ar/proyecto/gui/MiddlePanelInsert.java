1085ab1d4f647b9d4af2a1a727cf87cdafa948bf
Mabille <gabi.mabs@gmail.com>
2013-10-07 15:22:50 -0300

    Created API

--- DatabaseProyecto/src/ar/proyecto/gui/MiddlePanelInsert.java
+++ DatabaseProyecto/src/ar/proyecto/gui/MiddlePanelInsert.java
@@ -1,5 +1,35 @@
 package ar.proyecto.gui;
 
+import java.awt.Graphics;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class MiddlePanelInsert extends MiddlePanel {
+	private JLabel into;
+	
+	private JLabel nroPatente;
+	private JTextField txtNroPatente;
+	private JLabel typo;
+	private JTextField txtTypo;	
+	private JLabel modelo;
+	private JTextField txtModelo;	
+	private JLabel ano;
+	private JTextField txtAno;	
+
+	private JComboBox cboxMarca;
+	
+	
+	//constructor
+	public MiddlePanelInsert() {
+		super();		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
